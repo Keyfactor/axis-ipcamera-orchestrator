1d503812df284795365c75bfe484cd23784158d4
efiocca <efiocca@keyfactor.com>
2025-07-15 17:02:25 -0600

    Add logic to prevent Management-Add jobs on end entity certs

--- AxisIPCamera/Management.cs
+++ AxisIPCamera/Management.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Keyfactor.Logging;
 using Keyfactor.Orchestrators.Extensions;
 using Keyfactor.Orchestrators.Common.Enums;
@@ -42,79 +43,114 @@ namespace Keyfactor.Extensions.Orchestrator.AxisIPCamera
             //
             // config.JobCertificate.EntryContents - Base64 encoded string representation (PKCS12 if private key is included, DER if not) of the certificate to add for Management-Add jobs.
             // config.JobCertificate.Alias - optional string value of certificate alias (used in java keystores and some other store types)
-            // config.OpeerationType - enumeration representing function with job type.  Used only with Management jobs where this value determines whether the Management job is a CREATE/ADD/REMOVE job.
+            // config.OperationType - enumeration representing function with job type.  Used only with Management jobs where this value determines whether the Management job is a CREATE/ADD/REMOVE job.
             // config.Overwrite - Boolean value telling the Orchestrator Extension whether to overwrite an existing certificate in a store.  How you determine whether a certificate is "the same" as the one provided is AnyAgent implementation dependent
             // config.JobCertificate.PrivateKeyPassword - For a Management Add job, if the certificate being added includes the private key (therefore, a pfx is passed in config.JobCertificate.EntryContents), this will be the password for the pfx.
 
 
             //NLog Logging to c:\CMS\Logs\CMS_Agent_Log.txt
-
+            
             try
             {
                 _logger.MethodEntry();
-                _logger.LogDebug($"Begin Management...");
+                _logger.LogTrace($"Begin Management for Client Machine {config.CertificateStoreDetails.ClientMachine}...");
                 
                 //Management jobs, unlike Discovery, Inventory, and Reenrollment jobs can have 3 different purposes:
                 switch (config.OperationType)
                 {
                     case CertStoreOperationType.Add:
+                    {
                         //OperationType == Add - Add a certificate to the certificate store passed in the config object
                         //Code logic to:
                         // 1) Connect to the orchestrated server (config.CertificateStoreDetails.ClientMachine) containing the certificate store
                         // 2) Custom logic to add certificate to certificate store (config.CertificateStoreDetails.StorePath) possibly using alias as an identifier if applicable (config.JobCertificate.Alias).  Use alias and overwrite flag (config.Overwrite)
                         //     to determine if job should overwrite an existing certificate in the store, for example a renewal.
-                        
+
                         // Retrieve management config from Command
                         _logger.LogDebug($"Management Config {JsonConvert.SerializeObject(config)}");
                         _logger.LogDebug($"Client Machine: {config.CertificateStoreDetails.ClientMachine}");
-                        
-                        // Create client to connect to device
-                        _logger.LogTrace("Creating Api Rest Client...");
-                        var client = new AxisHttpClient(config, config.CertificateStoreDetails);
-                        _logger.LogTrace("Api Rest Client Created...");
-                        
+
                         // Get needed information from config
                         string alias = config.JobCertificate.Alias;
                         bool overwrite = config.Overwrite;
                         string certBase64Der = config.JobCertificate.Contents;
 
                         _logger.LogDebug($"Certificate contents:{certBase64Der}");
-                        
+
                         // TODO: Add logic to distinguish between CA cert and end entity certs
-                        
+                        // End-entity (client) certs will have private keys; therefore, do not continue with add 
+                        // if the cert contains a private key --- only add CA certs, which will not have a private key
+                        if (IsCACertificate(certBase64Der))
+                        {
+                            _logger.LogInformation("Certificate is a CA trust cert. Proceeding with Add operation...");
+                        }
+                        else
+                        {
+                            _logger.LogWarning("Certificate is an end-entity cert. Unable to add this certificate type to a device.");
+                            return new JobResult()
+                            {
+                                Result = OrchestratorJobStatusJobResult.Warning, 
+                                JobHistoryId = config.JobHistoryId,
+                                FailureMessage = $"UNSUPPORTED OPERATION --- This certificate cannot be used as a Trust. Unable to add end-entity certificates to a device."
+                            };
+                        }
+
                         // TODO: Add logic to handle overwrite flag
+                        
+                        
+                        // Create client to connect to device
+                        _logger.LogTrace("Creating Api Rest Client...");
+                        var client = new AxisHttpClient(config, config.CertificateStoreDetails);
+                        _logger.LogTrace("Api Rest Client Created...");
 
                         // Build PEM content
                         // TODO: Add this and the logic in reenrollment to client class (consolidate)
                         string formattedDer = InsertLineBreaks(certBase64Der, 64);
-                        _logger.LogDebug(($"Formatted certificate contents: {formattedDer}"));
-                        
+                        _logger.LogDebug(($"Formatted certificate contents:\n {formattedDer}"));
+
                         StringBuilder pemBuilder = new StringBuilder();
                         pemBuilder.Append(@"-----BEGIN CERTIFICATE-----\n");
-                        var noLineBreaks = formattedDer.Replace("\n",@"\n");
+                        var noLineBreaks = formattedDer.Replace("\n", @"\n");
                         pemBuilder.Append(noLineBreaks);
                         pemBuilder.Append(@"\n-----END CERTIFICATE-----");
                         var pemCert = pemBuilder.ToString();
-                        
+
                         // Add certificate with alias to the device
-                        client.AddCertificate(alias,pemCert);
-                        
-                        
-                        
+                        client.AddCACertificate(alias, pemCert);
+
                         break;
+                    }
                     case CertStoreOperationType.Remove:
+                    {
                         //OperationType == Remove - Delete a certificate from the certificate store passed in the config object
                         //Code logic to:
                         // 1) Connect to the orchestrated server (config.CertificateStoreDetails.ClientMachine) containing the certificate store
                         // 2) Custom logic to remove the certificate in a certificate store (config.CertificateStoreDetails.StorePath), possibly using alias (config.JobCertificate.Alias) or certificate thumbprint to identify the certificate (implementation dependent)
-                        // TODO: This is not supported operation
+
+                        // Retrieve management config from Command
+                        _logger.LogDebug($"Management Config {JsonConvert.SerializeObject(config)}");
+                        _logger.LogDebug($"Client Machine: {config.CertificateStoreDetails.ClientMachine}");
+
+                        // Create client to connect to device
+                        _logger.LogTrace("Creating Api Rest Client...");
+                        var client = new AxisHttpClient(config, config.CertificateStoreDetails);
+                        _logger.LogTrace("Api Rest Client Created...");
+
+                        // Get needed information from config
+                        string alias = config.JobCertificate.Alias;
+
+                        // Remove certificate with alias from the device
+
+
                         break;
+                    }
                     case CertStoreOperationType.Create:
                         //OperationType == Create - Create an empty certificate store in the provided location
                         //Code logic to:
                         // 1) Connect to the orchestrated server (config.CertificateStoreDetails.ClientMachine) where the certificate store (config.CertificateStoreDetails.StorePath) will be located
                         // 2) Custom logic to first check if the store already exists and add it if not.  If it already exists, implementation dependent as to how to handle - error, warning, success
-                        // TODO: This is not a supported operation
+                        // TODO: This is not supported operation
+
                         break;
                     default:
                         //Invalid OperationType.  Return error.  Should never happen though
@@ -124,7 +160,7 @@ namespace Keyfactor.Extensions.Orchestrator.AxisIPCamera
             catch (Exception ex)
             {
                 //Status: 2=Success, 3=Warning, 4=Error
-                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = "Custom message you want to show to show up as the error message in Job History in KF Command" };
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = $"Management Job Failed During '{config.OperationType.ToString()}' Operation: {ex.Message} - Refer to logs for more detailed information." };
             }
 
             //Status: 2=Success, 3=Warning, 4=Error
@@ -155,6 +191,36 @@ namespace Keyfactor.Extensions.Orchestrator.AxisIPCamera
             }
 
             return new string(result);
+        }
+
+        /// <summary>
+        /// ASSUMPTION: This function assumes a certificate to be an end entity certificate
+        /// if the basic constraints extension is NOT present in a version 3 certificate.
+        /// FALLBACK: If this check produces a false positive, the Axis API will fail on the
+        /// HTTP request and details will be logged accordingly.
+        /// </summary>
+        /// <param name="certBase64Der">Cert contents to add represented as Base-64 encoded DER</param>
+        /// <returns>True if CA cert; False otherwise</returns>
+        private static bool IsCACertificate(string certBase64Der)
+        {
+            // Convert the cert contents to a byte array
+            byte[] certBytes = Convert.FromBase64String(certBase64Der);
+                        
+            // Create an X509 object so we can analyze the contents
+            X509Certificate2 certToAdd = new X509Certificate2(certBytes);
+
+            foreach (X509Extension ext in certToAdd.Extensions)
+            {
+                if (ext.Oid?.Value == "2.5.29.19") // Indicates if the subject may act as a CA, with public key used to verify cert signatures
+                {
+                    if (ext is X509BasicConstraintsExtension basicConstraints)
+                    {
+                        return basicConstraints.CertificateAuthority;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
